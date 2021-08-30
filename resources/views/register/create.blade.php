91c91e89fcd7644271a93bb60d7d11347fb32436
Lewicv83 <lewicv83@gmail.com>
2021-08-30 14:10:33 -0400

    Set up register view and register controller

---
+++ resources/views/register/create.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<title>Animal Combat App</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+
+    <section class="px-6 py-8">
+        <main class="max-w-lg mx-auto bg-gray-300 p-6 rounded-xl">
+
+            <h3 style="text-align:center">Register for</h3>    
+            <h1 style="text-align:center ">ANIMAL COMBAT</h1>
+
+            <form method="POST" action="/register" class="mt-10">
+
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                        for="username"
+                    >
+                        Username
+                    </label>
+
+                    <input class="border border-gray-400 p-2 w-full"
+                        type="text"
+                        name="username"
+                        id="username"
+                        required=""
+
+                    >
+
+                </div>
+            </form>
+
+
+        </main>
+    </section>
+
+</body>
